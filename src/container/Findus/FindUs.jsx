4e00c4a855d14304cb2ca84998fdf4fbe0e56578
adnanali000 <adnan.museel123@gmail.com>
2022-02-17 00:47:43 +0500

    footer work done

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -3,22 +3,23 @@ import {images} from '../../constants'
 import {SubHeading} from '../../components'
 
 const FindUs = () => (
-  <div className="app__bg app__wrapper section__padding" id='contact'>
-    <div className="app__wrapper_info">
-        <SubHeading title="Contact" />
-        <h1 className="headtext__cormorant" style={{marginBottom:'3rem'}}>Find Us</h1>
-        <div className="app__wrapper-content">
-          <p className="p__opensans">Address: Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <p className="p__cormorant" style={{color:'DCCA87',margin:'2rem 0'}}>Opening Hours</p>
-          <p className="p__opensans">Mon - Fri: 10:00 am - 1:00 am</p>
-          <p className="p__opensans">Sat - Sun: 10:00 am - 3:00 am</p>
-        </div>
-        <button type="button" className='custom__button' style={{marginTop:'2rem'}}>Visit Us</button>
+  <div className="app__bg app__wrapper section__padding" id="contact">
+  <div className="app__wrapper_info">
+    <SubHeading title="Contact" />
+    <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>Find Us</h1>
+    <div className="app__wrapper-content">
+      <p className="p__opensans">Address: Lorem ipsum dolor sit amet consectetur.</p>
+      <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
+      <p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
+      <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
     </div>
-    <div className="app__wrapper_img">
-        <img src={images.findus} alt="find-Us" />
-    </div>
+    <button type="button" className="custom__button" style={{ marginTop: '2rem' }}>Visit Us</button>
   </div>
+
+  <div className="app__wrapper_img">
+    <img src={images.findus} alt="finus_img" />
+  </div>
+</div>
 );
 
 export default FindUs;
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
-
+import {FooterOverlay,Newsletter} from '../../components'
+import {FiFacebook,FiTwitter,FiInstagram} from 'react-icons/fi'
 import './Footer.css';
+import {images} from '../../constants'
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+    <FooterOverlay />
+    <Newsletter />
+  
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">Address: Lorem ipsum dolor sit amet.</p>
+        <p className="p__opensans">09876543210</p>
+        <p className="p__opensans">01234567899</p>
+      </div>
+      <div className="app__footer-links_logo">
+          <img src={images.gericht} alt="footerLogo" />
+          <p className="p__opensans">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur numquam voluptatum ea."</p>
+          <img src={images.spoon} alt="footerSpoon"  style={{marginTop:15}}/>
+          <div className="app__footer-links_icons">
+              <FiFacebook />
+              <FiInstagram />  
+              <FiTwitter />  
+          </div>     
+      </div>
+      <div className="app__footer-links_work">
+      <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      </div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">2022 Gericht. All Rights reserved.</p>
+    </div>
   </div>
 );
 
